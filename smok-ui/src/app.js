cbf58db498f63ff8b807b93474c64a6787c47223
Adam Bien <abien@adam-bien.com>
2020-10-14 11:59:49 +0200

    lit-html introduced

--- smok-ui/src/app.js
+++ smok-ui/src/app.js
@@ -1,12 +1,19 @@
+import { html, render } from "./lit-html.js";
+
 class Smok extends HTMLElement { 
 
     connectedCallback() {
         const message = "some fire"
-        this.innerHTML = `
-            <button>spit fire</button>
+        const template = html`
+            <button @click=${_=> this.spitFire()}>spit fire</button>
             <h2>Smok ${this.getAttribute('message')} from Wawel with ${message}</h2>
         `;
-        this.querySelector('button').onclick = _ => console.log("smok spits fire");
+        console.log(template);        
+        render(template,this);
+    }
+
+    spitFire() { 
+        console.log("spitting like crazy");
     }
 
 }
